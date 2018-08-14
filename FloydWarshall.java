840542e799d7a94b875bad24601ce7274d0fb43c
Mickael Figueredo <mickaelfigueredo@gmail.com>
2018-08-14 15:55:00 -0300

    First Commit

---
+++ FloydWarshall.java
@@ -0,0 +1,120 @@
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+
+
+public class FloydWarshall
+{
+  private static final int INFINITY = 9999999;
+  int[][] d = null;
+  
+  //LISTA DE MATRIZES DE PESOS
+  ArrayList<int[][]> D = new ArrayList();
+  //LISTA DE MATRIZES DE PREDECESSORES
+  ArrayList<int[][]> Shortest = new ArrayList();
+  
+  //CONSTRUTOR
+  public FloydWarshall(int[][] paramArrayOfInt)
+  {
+    d = paramArrayOfInt;
+  }
+  
+
+  //METODO PRINCIPAL PARA CALCULAR PESOS 
+  public int[][] getFloyWarshall(int rows)
+  {
+      int n = rows;
+      D.add(0, d);
+      Shortest.add(buildShortestPath(0, rows));
+      for(int k = 1 ; k<=n ; k++)
+      {
+          int[][] aux = new int[rows+1][rows+1];
+          for (int i = 1; i<=n ; i++)
+          {
+              for(int j  = 1 ; j<=n ; j++)
+              {
+                  aux[i][j] = Math.min(D.get(k-1)[i][j], D.get(k-1)[i][k]+D.get(k-1)[k][j]);
+              }
+          }
+          D.add(k, aux);
+          Shortest.add(buildShortestPath(k, rows));
+      }
+  
+      return D.get(n);
+  }
+
+  //METODO PARA CONSTRUIR MATRIZES DE PREDECESSORES
+  public int[][] buildShortestPath(int k, int rows)
+  {
+      int n = rows;
+      int[][] path = new int[rows+1][rows+1];
+      if(k==0)
+      {
+          for (int i = 1; i<=n ; i++)
+          {
+              for(int j  = 1 ; j<=n ; j++)
+              {
+                  if(i==j || D.get(k)[i][j]==INFINITY)
+                  {
+                      path[i][j] = INFINITY;
+                  }else{
+                      path[i][j] = i;
+                  }
+              }
+          }            
+      }else{
+          for (int i = 1; i<=n ; i++)
+          {
+              for(int j  = 1 ; j<=n ; j++)
+              {
+                  if(D.get(k-1)[i][j]<=D.get(k-1)[i][k]+D.get(k-1)[k][j])
+                  {
+                      path[i][j] = Shortest.get(k-1)[i][j];
+                  }else{
+                      path[i][j] = Shortest.get(k-1)[k][j];
+                  }
+              }
+          }      
+      }
+
+      return path;
+
+  }
+  
+
+  //METODO PARA VERIFICAR CICLOS NEGATIVOS
+  public boolean isThereNegativeCicle(int size)
+  {
+    for (int i = 1; i <= size; i++)
+    {
+      if (D.get(size)[i][i] != 0)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  //METODO PARA MOSTRAR O CAMINHO MAIS CURTO ENTRE DOIS VERTICES
+  public void printAllPairsShortestPath(int i, int j, int size)
+  {
+    if (i == j)
+    {
+      System.out.print(i + "->");
+    } else if (Shortest.get(size)[i][j] != 9999999)
+    {
+      printAllPairsShortestPath(i, Shortest.get(size)[i][j], size);
+      System.out.print(j + "->");
+    }
+  }
+  
+  //METODOS PARA RETORNAR LISTA DE MATRIZES
+  public ArrayList<int[][]> getFloydWArrayList()
+  {
+    return this.D;
+  }
+  
+  public ArrayList<int[][]> getShortestPath() {
+    return this.Shortest;
+  }
+}
---
+++ Main.java
@@ -0,0 +1,187 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+
+public class Main
+{
+  public static int[][] d = null;
+  public static int size;
+  
+  public static void main(String[] args) {
+    ArrayList<int[][]> floyd = null;
+    ArrayList<int[][]> shortest = null;
+    
+    //LE GRAFO DO ARQUIVO
+    readMatrix(args[1]);
+    
+    //CRIA ELEMENTO QUE USARA O ALGORITMO FLOYWARSHALL
+    FloydWarshall f = new FloydWarshall(d);
+    
+    //COMPUTA OS PESOS
+    f.getFloyWarshall(size);
+    
+    //PEGA LISTA DE MATRIZES DE PESOS E CAMINHOS MAIS CURTOS
+    floyd = f.getFloydWArrayList();
+    shortest = f.getShortestPath();
+    
+    //VERIFICA SE HA CICLOS NEGATIVOS
+    if (!f.isThereNegativeCicle(size))
+    {
+      //METODO 1 PARA MOSTRAR LISTAS
+      if (args[0].equals("1"))
+      {
+        prinMatrixList(floyd, shortest, size);
+      }
+      //METODO 2 PARA MOSTRAR RESULTADOS FINAIS
+      else if (args[0].equals("2")) 
+      {
+        printResult(floyd.get(size), shortest.get(size), size);
+      }
+      //METODO 3 PARA MOSTRAR OS CAMINHOS DE TODOS PARA TODOS
+      else if (args[0].equals("3"))
+      {
+        for (int i = 1; i <= size; i++)
+        {
+          for (int j = 1; j <= size; j++)
+          {
+            f.printAllPairsShortestPath(i, j, size);
+            System.out.print("\n");
+          }
+        }
+      }
+    }
+    else {
+      System.out.println("Desculpe, existe um ou mais ciclos negativos no grafo.");
+    }
+  }
+  
+
+
+
+  // ##########################
+  // ##########################
+  // ######### FUNCOES ########
+  // ######## AUXILIARES ######
+  // ##########################
+
+
+
+  //FUNCAO PARA LEITURA DO ARQUIVO
+  public static void readMatrix(String paramString)
+  {
+    FileReader localFileReader = null;
+    Object localObject = null;
+    BufferedReader localBufferedReader = null;
+    
+
+    String path = paramString;
+    
+
+    try
+    {
+      localFileReader = new FileReader(path);
+      localBufferedReader = new BufferedReader(localFileReader);
+      
+      size = Integer.parseInt(localBufferedReader.readLine());
+      
+      d = new int[size + 1][size + 1];
+      
+
+
+      int i = 1;
+      String line; 
+      while ((line = localBufferedReader.readLine()) != null)
+      {
+        String[] elements = line.split(" ");
+        for (int j = 1; j <= size; j++) {
+          if (elements[(j - 1)].equals("INF")) {
+            d[i][j] = 9999999;
+          } else {
+            d[i][j] = Integer.parseInt(elements[(j - 1)]);
+          }
+        }
+        
+        i++;
+      }
+    }
+    catch (Exception localException) {
+      localException.printStackTrace();
+    }
+  }
+  //FUNCAO PARA PRINTAR MATRIZES PRINCIPAIS
+  public static void printResult(int[][] a, int[][] b, int size)
+  {
+    System.out.print("############## FLOYD WARSHALS WEIGTHS ##############");
+    for (int i = 1; i <= size; i++)
+    {
+      System.out.print("\n");
+      for (int j = 1; j <= size; j++)
+      {
+        if (a[i][j] == 9999999) {
+          System.out.print(" ∞ ");
+        } else {
+          System.out.print(" " + a[i][j] + " ");
+        }
+      }
+    }
+    System.out.print("\n");
+    System.out.print("############## SHORTEST PATH ##############");
+    for (int i = 1; i <= size; i++)
+    {
+      System.out.print("\n");
+      for (int j = 1; j <= size; j++)
+      {
+        if (b[i][j] == 9999999) {
+          System.out.print(" NIL ");
+        } else {
+          System.out.print(" " + b[i][j] + " ");
+        }
+      }
+    }
+    System.out.print("\n");
+  }
+  //FUNCAO PARA MOSTRAR LISTA DE MATRIZES
+  public static void prinMatrixList(ArrayList<int[][]> a, ArrayList<int[][]> b, int size)
+  {
+    System.out.print("############## FLOYD WARSHALS WEIGTHS ##############");
+    for (int k = 1; k <= size; k++)
+    {
+
+      for (int i = 1; i <= size; i++)
+      {
+        System.out.print("\n");
+        for (int j = 1; j <= size; j++)
+        {
+          if (a.get(k)[i][j] == 9999999) 
+          {
+            System.out.print(" ∞ ");
+          } else {
+            System.out.print(" " +a.get(k)[i][j] + " ");
+          }
+        }
+      }
+      System.out.print("\n");
+    }
+    System.out.print("############## SHORTEST PATH ##############");
+    for (int k = 1; k <= size; k++)
+    {
+
+      for (int i = 1; i <= size; i++)
+      {
+        System.out.print("\n");
+        for (int j = 1; j <= size; j++)
+        {
+          if (b.get(k)[i][j] == 9999999) 
+          {
+            System.out.print(" NIL ");
+          } else {
+            System.out.print(" " + b.get(k)[i][j] + " ");
+          }
+        }
+      }
+      System.out.print("\n");
+    }
+  }
+}
